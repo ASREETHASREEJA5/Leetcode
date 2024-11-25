5666096b557b3b3f74cc6efefa51a05c9bf58ebe
GOLLA ASREETHA SREEJA <155885374+ASREETHASREEJA5@users.noreply.github.com>
2024-11-25 18:06:41 +0530

    Time: 31 ms (77.24%) | Memory: 16.8 MB (8.03%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.py
@@ -0,0 +1,9 @@
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        s = ""
+        for i in address:
+            if i == ".":
+                s+="[.]"
+            else:
+                s+=i
+        return s        
