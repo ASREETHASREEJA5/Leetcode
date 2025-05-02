bd3eb2a3f666784f052857e34069e9ba2b0ec053
GOLLA ASREETHA SREEJA <155885374+ASREETHASREEJA5@users.noreply.github.com>
2025-05-02 21:17:39 +0530

    Time: 308 ms (32.45%) | Memory: 18.3 MB (45.87%) - LeetSync

---
+++ 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        l = {}
+        for i in range(lo,hi+1):
+            c = 0
+            n = i
+            while n != 1:
+                if n%2 == 0:
+                    n = n//2
+                else:
+                    n = 3*n+1
+                c+=1
+            l[i] = c
+        sorted_items = sorted(l.items(), key=lambda x: (x[1], x[0]))
+        print(sorted_items)
+        return sorted_items[k - 1][0]
+            
