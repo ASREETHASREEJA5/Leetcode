bdc722ee3f015caa8d2a7f3bac7550ec5e9aeb27
GOLLA ASREETHA SREEJA <155885374+ASREETHASREEJA5@users.noreply.github.com>
2025-05-08 13:41:29 +0530

    Time: 3 ms (64.32%) | Memory: 17.7 MB (61.23%) - LeetSync

--- 2083-three-divisors/three-divisors.py
+++ 2083-three-divisors/three-divisors.py
@@ -1,7 +1,8 @@
+import math 
 class Solution:
     def isThree(self, n: int) -> bool:
-        c = 0
-        for i in range(1,n+1):
+        c = 1
+        for i in range(1,(n//2)+1):
             if n%i == 0:
                 c+=1
         if c==3:
