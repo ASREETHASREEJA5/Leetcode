33c3487aa1071fddfa7591949564483c2050cc2a
GOLLA ASREETHA SREEJA <155885374+ASREETHASREEJA5@users.noreply.github.com>
2025-05-10 21:29:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (31.16%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        def helper(node, lower, upper):
+            if not node:
+                return True
+
+            val = node.val
+            if val <= lower or val >= upper:
+                return False
+
+            if not helper(node.right, val, upper):
+                return False
+            if not helper(node.left, lower, val):
+                return False
+
+            return True
+
+        return helper(root, float('-inf'), float('inf'))
