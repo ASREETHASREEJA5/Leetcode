ad64bdd88815929b55cb5a247d0973862c37e507
GOLLA ASREETHA SREEJA <155885374+ASREETHASREEJA5@users.noreply.github.com>
2025-09-08 21:34:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (78.40%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<=2:
+            return n
+        a,b=1,1
+        for i in range(2,n+1):
+            a,b=b,a+b
+        return b
+        
