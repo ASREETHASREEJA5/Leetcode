8b148cc0d383e5055eaa25bb89a284dca6f852b2
GOLLA ASREETHA SREEJA <155885374+ASREETHASREEJA5@users.noreply.github.com>
2025-10-06 19:28:05 +0530

    Time: 51 ms (64.09%) | Memory: 38.1 MB (34.21%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        mod_map = {0: -1}  # remainder → earliest index
+        prefix_sum = 0
+
+        for i, num in enumerate(nums):
+            prefix_sum += num
+            mod = prefix_sum % k if k != 0 else prefix_sum 
+            if mod in mod_map:
+                if i - mod_map[mod] > 1:
+                    return True
+            else:
+                mod_map[mod] = i
+
+        return False
