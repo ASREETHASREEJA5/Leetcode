a5879f15441c1d6d5550dc52513d7b12072e6d02
GOLLA ASREETHA SREEJA <155885374+ASREETHASREEJA5@users.noreply.github.com>
2025-10-08 21:47:54 +0530

    Time: 4 ms (78.85%) | Memory: 20.7 MB (62.73%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,11 +1,32 @@
+from typing import List
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        r = len(nums)/3
-        l1 = list(set(nums))
-        l= []
-        for i in l1:
-            if nums.count(i)>r:
-                l.append(i)
-        return l
+        if not nums:
+            return []
 
-        
+        # Step 1: Find two potential candidates
+        cand1 = cand2 = None
+        cnt1 = cnt2 = 0
+
+        for num in nums:
+            if num == cand1:
+                cnt1 += 1
+            elif num == cand2:
+                cnt2 += 1
+            elif cnt1 == 0:
+                cand1, cnt1 = num, 1
+            elif cnt2 == 0:
+                cand2, cnt2 = num, 1
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
+
+        # Step 2: Verify actual counts
+        res = []
+        n = len(nums)
+        for c in [cand1, cand2]:
+            if c is not None and nums.count(c) > n // 3:
+                res.append(c)
+
+        return res
